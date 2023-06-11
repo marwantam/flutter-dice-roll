39592b5f0267ab6ad02239cec9d978f1e624ba08
marwantam <marwan.tam18@gmail.com>
2023-06-11 10:40:01 +0300

    first

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purpleAccent,
+                Colors.blueAccent,
+                Colors.deepPurple
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
